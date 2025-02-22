06afeb07bbd7bcf1cf0f13833608db6ebe2fafe2
Sam Michnik <michniksamuel@gmail.com>
2025-02-22 17:37:40 -0500

    changed ai prompt

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -3,92 +3,97 @@ import OpenAI from "openai";
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 const audioFiles = [
-	{
-		length: "15 minutes",
-		embedCode: '<iframe src="https://drive.google.com/file/d/1ZTHiTs9lEpQ9X4urk40A2CGmDrWnbsLj/preview" width="320" height="240" allow="autoplay"></iframe>'
-	},
-	{
-		length: "3 minutes",
-		embedCode: '<iframe src="https://drive.google.com/file/d/1YvidjoxjwulcZozSnL3wL7najK6r6v3u/preview" width="320" height="240" allow="autoplay"></iframe>'
-	},
-	{
-		length: "8 minutes",
-		embedCode: '<iframe src="https://drive.google.com/file/d/1yaM_IyUszTpSvVgQLXT9GYLZ1TBXSlaZ/preview" width="320" height="240" allow="autoplay"></iframe>'
-	},
-	{
-		length: "20 minutes",
-		embedCode: '<iframe src="https://drive.google.com/file/d/1kRhZ77FqW6fSBJ-xF6BpgW-UMTrHGM5r/preview" width="320" height="240" allow="autoplay"></iframe>'
-	},
-	{
-		length: '5 minutes',
-		embedCode: '<iframe src="https://drive.google.com/file/d/1hOziUfUqsteyw3-ry-rJFcz9OffQq_3u/preview" width="320" height="240" allow="autoplay"></iframe>'
-	},
-	{
-		length: '10 minutes',
-		embedCode: '<iframe src="https://drive.google.com/file/d/1T90L7ZtyO6oFrurFwo1Ke3v-2imLeBPi/preview" width="320" height="240" allow="autoplay"></iframe>'
-	},
-	{
-		length: '7 minutes',
-		embedCode: '<iframe src="https://drive.google.com/file/d/17ilQ5Pv3kcORJSZybWa1gazyM5CMqdwn/preview" width="320" height="240" allow="autoplay"></iframe>'
-	},
-	{
-		length: '4 minutes',
-		embedCode: '<iframe src="https://drive.google.com/file/d/1MSjsjiTjenCt2W5tZsEgsGTnO5VpalVa/preview" width="320" height="240" allow="autoplay"></iframe>'
-	},
-	{
-		length: '10 minutes',
-		embedCode: '<iframe src="https://drive.google.com/file/d/19ght3L31tO7YhknFSnW00LlXIlpeKelo/preview" width="320" height="240" allow="autoplay"></iframe>'
-	}
-]
+  {
+    length: "15 minutes",
+    link: "https://drive.google.com/file/d/1ZTHiTs9lEpQ9X4urk40A2CGmDrWnbsLj/preview",
+  },
+  {
+    length: "3 minutes",
+    link: "https://drive.google.com/file/d/1YvidjoxjwulcZozSnL3wL7najK6r6v3u/preview",
+  },
+  {
+    length: "8 minutes",
+    link: "https://drive.google.com/file/d/1yaM_IyUszTpSvVgQLXT9GYLZ1TBXSlaZ/preview",
+  },
+  {
+    length: "20 minutes",
+    link: "https://drive.google.com/file/d/1kRhZ77FqW6fSBJ-xF6BpgW-UMTrHGM5r/preview",
+  },
+  {
+    length: "5 minutes",
+    link: "https://drive.google.com/file/d/1hOziUfUqsteyw3-ry-rJFcz9OffQq_3u/preview",
+  },
+  {
+    length: "10 minutes",
+    link: "https://drive.google.com/file/d/1T90L7ZtyO6oFrurFwo1Ke3v-2imLeBPi/preview",
+  },
+  {
+    length: "7 minutes",
+    link: "https://drive.google.com/file/d/17ilQ5Pv3kcORJSZybWa1gazyM5CMqdwn/preview",
+  },
+  {
+    length: "4 minutes",
+    link: "https://drive.google.com/file/d/1MSjsjiTjenCt2W5tZsEgsGTnO5VpalVa/preview",
+  },
+  {
+    length: "10 minutes",
+    link: "https://drive.google.com/file/d/19ght3L31tO7YhknFSnW00LlXIlpeKelo/preview",
+  },
+];
 
 const games = [
-	"https://cloud.onlinegames.io/games/2025/unity/voxel-world/index-og.html",
-	"https://www.onlinegames.io/games/2023/q2/dinosaur-game/index.html",
-	"https://www.onlinegames.io/games/2023/unity/hero-rush-tower-defense/index.html",
-	"https://www.onlinegames.io/games/2023/unity/hero-rush-tower-defense/index.html",
-	"https://www.onlinegames.io/games/2023/construct/234/among-impostor/index.html"
-]
+  "https://cloud.onlinegames.io/games/2025/unity/voxel-world/index-og.html",
+  "https://www.onlinegames.io/games/2023/q2/dinosaur-game/index.html",
+  "https://www.onlinegames.io/games/2023/unity/hero-rush-tower-defense/index.html",
+  "https://www.onlinegames.io/games/2023/unity/hero-rush-tower-defense/index.html",
+  "https://www.onlinegames.io/games/2023/construct/234/among-impostor/index.html",
+];
 
 const make_prompt = (args) => {
-
-	return `You are a frontend assistant using nextjs with tailwindcss. You will generate a user interface 
-	depending on how the user is feeling with different quotes, exercises, and games using jsx and tailwindcss styles. 
-	create a grid with 4 different components. 
-	
-	Component examples are: a heading that responds to the person's mood, a 
-	textbox for the user to write down their thoughts, a chatbot that the user can interact with and seek more advice, 
-	a meditation exercise the user can do to maybe redirect their focus, etc. The meditation exercise should embed an iframe with mp3, which is defined as such: 
-	
-	Given a spectrum of happy to sad, the more sad a person is, play a longer audio file.  Here are your options in json format:
+  return `You are a mental wellness assistant. Your job is to determine how negative or positive the user feels and provide the correct analysis on their mood.
+	Depending on their negativity levels (0-10, where 0 is they are not negative and 10 is severely negative, chose an audio embed based on their negativity level. If their negativity levels are above a 3, here are your options in json format:
 	${JSON.stringify(audioFiles)}
+	The higher the score, the longer the audio should be with respect to the score.
 
-	E.g., if a person is extremely depressed, use the embedCode from the object with the longest 'length'.
-	
-	only provide the code with no other messages or formatting. Put this into a div, with inline css.
-	`
-}
-export async function POST(req) {
-	try {
-		const { mood } = await req.json();
+	If their negativity levels are below a 2, choose a game from the array: ${games}.
 
-		const response = await openai.chat.completions.create({
-			model: "gpt-4o",
-			messages: [
-				{
-					role: "system", content: make_prompt()
-				},
-				{ role: "user", content: `I am feeling ${mood}` },
-			],
-			temperature: 0.7,
-			max_tokens: 500,
-		});
+	provide your output in the json format below (do not provide any explanations):
 
-		const generatedCode = response.choices[0].message.content.replace('```jsx', '').replace('```', '');
-
-		return Response.json({ generatedCode });
-	} catch (error) {
-		console.error("OpenAI API error:", error);
-		return Response.json({ message: "Failed to generate UI." }, { status: 500 });
+	{
+		negativity_score: int,
+		meditation_audio: null if negativity_score < 3 else string,
+		video_game: null if negativity_score >= 3 else string,
+		motivational_quote: string,
 	}
-}
+	`;
+};
+export async function POST(req) {
+  try {
+    const { mood } = await req.json();
 
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o",
+      messages: [
+        {
+          role: "system",
+          content: make_prompt(),
+        },
+        { role: "user", content: `I am feeling ${mood}` },
+      ],
+      temperature: 0.7,
+      max_tokens: 500,
+    });
+
+    const generatedCode = response.choices[0].message.content
+      .replace("```jsx", "")
+      .replace("```", "");
+
+    return Response.json({ generatedCode });
+  } catch (error) {
+    console.error("OpenAI API error:", error);
+    return Response.json(
+      { message: "Failed to generate UI." },
+      { status: 500 }
+    );
+  }
+}
